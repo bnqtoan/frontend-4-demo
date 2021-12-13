efe4dac098beb205258a68288ed366d2e350a7dc
Toan Bui (Flexer) <bnqtoan@gmail.com>
2021-12-13 20:11:46 +0700

    add tailwind

---
+++ 03-tailwind/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "**/*.html"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      body: "'Inter', sans-serif"
+    },
+    extend: {
+      colors: {
+        gray: {
+          100: '#F4F5F7'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
